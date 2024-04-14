3aa87753ca113826e1a040fc5cdc32012ec7746e
Tomas Cadete <t.cadete24@gmail.com>
2024-04-14 21:15:30 +0100

    Publishing occupancy grid map with LiDAR and camera data

--- amr_sim/tracker.py
+++ amr_sim/tracker.py
@@ -4,15 +4,71 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid, Odometry
 import transforms3d
+from amr_interfaces.msg import Object
+# Object has the following fields (coordinates in the world frame):
+# string type
+# float64 x
+# float64 y
+# float64 z
+
+
+# Define a class to represent a tracked object
+# It should only contain the object's id, type, x, and y coordinates, as well as a steps_since_seen counter
+class TrackedObject:
+    def __init__(self):
+        self.id = 0
+        self.type = ''
+        self.x = 0.0
+        self.y = 0.0
+        self.steps_since_seen = 0
+
+
 
 class ObjectTracker(Node):
     def __init__(self):
         super().__init__('object_tracker')
         self.lidar_subscription = self.create_subscription(LaserScan, '/scan', self.lidar_callback, 10)
         self.odom_subscription = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+        self.objects_subscription = self.create_subscription(Object, '/detections', self.incoming_object_callback, 10)
         self.publisher = self.create_publisher(OccupancyGrid, '/occupancy_grid', 10)
         self.robot_odom = None
+        self.tracked_objects = []
+        self.tracked_objects_id = 1
         self.initialize_occupancy_grid()
+
+
+    def incoming_object_callback(self, msg):
+        if msg.type == 'None':
+            return
+
+        new_object = True
+        for obj in self.tracked_objects:
+            distance = np.sqrt((obj.x - msg.x)**2 + (obj.y - msg.y)**2)
+            if distance < 1.0:
+                obj.x = msg.x
+                obj.y = msg.y
+                obj.steps_since_seen = 0
+                new_object = False
+                break
+
+        if new_object:
+            obj = TrackedObject()
+            obj.id = self.tracked_objects_id
+            self.tracked_objects_id += 1
+            obj.type = msg.type
+            obj.x = msg.x
+            obj.y = msg.y
+            obj.steps_since_seen = 0
+            self.tracked_objects.append(obj)
+
+        # Remove objects that haven't been seen for a while
+        for obj in self.tracked_objects:
+            obj.steps_since_seen += 1
+            if obj.steps_since_seen > 100:
+                self.tracked_objects.remove(obj)
+
+
+
 
     def odom_callback(self, msg):
         self.robot_odom = msg
@@ -28,8 +84,6 @@ class ObjectTracker(Node):
         orientation_q = self.robot_odom.pose.pose.orientation
         _, _, yaw = transforms3d.euler.quat2euler([orientation_q.w, orientation_q.x, orientation_q.y, orientation_q.z], axes='sxyz')
 
-        self.get_logger().info(f'Robot position: ({robot_x}, {robot_y})')
-        self.get_logger().info(f'Robot orientation: {yaw} radians')
 
         # Compute global coordinates for each LIDAR point
         ranges = np.array(msg.ranges)
@@ -46,12 +100,24 @@ class ObjectTracker(Node):
         # Clear the occupancy grid
         self.occupancy_grid.data = [0] * (self.occupancy_grid.info.width * self.occupancy_grid.info.height)
 
-        # Update the occupancy grid
+        # Update the occupancy grid with the LIDAR data
         for x, y in zip(global_x, global_y):
             grid_x = int((x - self.occupancy_grid.info.origin.position.x) / self.occupancy_grid.info.resolution)
             grid_y = int((y - self.occupancy_grid.info.origin.position.y) / self.occupancy_grid.info.resolution)
             if 0 <= grid_x < self.occupancy_grid.info.width and 0 <= grid_y < self.occupancy_grid.info.height:
                 self.occupancy_grid.data[grid_y * self.occupancy_grid.info.width + grid_x] = 100
+
+        # Update the occupancy grid with the tracked objects
+        # Both the occupancy grid and the coordinates of the objects are in the world frame
+        # Each object is represented with a 2m x 2m square centered at its coordinates
+        for obj in self.tracked_objects:
+            grid_x = int((obj.x - self.occupancy_grid.info.origin.position.x) / self.occupancy_grid.info.resolution)
+            grid_y = int((obj.y - self.occupancy_grid.info.origin.position.y) / self.occupancy_grid.info.resolution)
+            if 0 <= grid_x < self.occupancy_grid.info.width and 0 <= grid_y < self.occupancy_grid.info.height:
+                for i in range(-2, 3):
+                    for j in range(-2, 3):
+                        if 0 <= grid_x + i < self.occupancy_grid.info.width and 0 <= grid_y + j < self.occupancy_grid.info.height:
+                            self.occupancy_grid.data[(grid_y + j) * self.occupancy_grid.info.width + grid_x + i] = 100
 
         self.occupancy_grid.header.stamp = self.get_clock().now().to_msg()
         self.publisher.publish(self.occupancy_grid)
