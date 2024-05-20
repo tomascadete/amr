d4e3ab4f4e7b5e2e06c7f1d73507cef26fa8c6b8
Tomas Cadete <t.cadete24@gmail.com>
2024-05-20 16:16:17 +0100

    Robot now stops if it detects a possible collision with an object on the road and waits until the path is clear

--- amr_sim/crossing.py
+++ amr_sim/crossing.py
@@ -1,9 +1,7 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
 from nav_msgs.msg import Odometry, OccupancyGrid, Path
-from geometry_msgs.msg import Twist, PoseStamped
-import cv2
+from geometry_msgs.msg import PoseStamped
 from ultralytics import YOLO
 import numpy as np
 from sklearn.cluster import DBSCAN
@@ -11,7 +9,6 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from cv_bridge import CvBridge, CvBridgeError
 from amr_interfaces.msg import LightColour
-
 
 
 class TrackedObject:
@@ -27,21 +24,23 @@ class TrackedObject:
         self.steps_since_seen = 0
         self.distances_to_robot = []
         self.is_approaching = True
+        self.predicted_positions = []
 
     def add_position(self, position, time):
         self.positions.append(position)
         self.timestamps.append(time)
-
+        self.predict_future_positions()
 
     def update_velocity_and_acceleration(self, time_diff):
         if len(self.positions) > 1:
-            initial_position = self.positions[0]
-            last_position = self.positions[-1]
-            initial_time = self.timestamps[0]
-            last_time = self.timestamps[-1]
+            recent_positions = self.positions[-20:]
+            recent_timestamps = self.timestamps[-20:]
+            last_position = recent_positions[-1]
+            last_time = recent_timestamps[-1]
+            initial_position = recent_positions[0]
+            initial_time = recent_timestamps[0]
             time_diff = last_time - initial_time
             if time_diff > 0:
-                # Consider only displacement along the y-axis
                 displacement = last_position[1] - initial_position[1]
                 velocity = displacement / time_diff
                 self.velocities.append(velocity)
@@ -60,12 +59,26 @@ class TrackedObject:
         else:
             self.acceleration = 0.0
 
-
-    def calculate_weighted_average(self,data):
-        weights = np.exp(-0.5*np.arange(len(data))) # The higher the decay rate, the more recent values will be weighted more heavily
+    def calculate_weighted_average(self, data):
+        weights = np.exp(-0.1 * np.arange(len(data)))
         return np.average(data[-10:], weights=weights[-10:])
-            
 
+    def predict_future_positions(self):
+        if len(self.positions) < 2:
+            return
+
+        times = np.array(self.timestamps).reshape(-1, 1)
+        positions = np.array(self.positions)
+
+        model_y = LinearRegression().fit(times, positions[:, 1])
+
+        future_times = np.linspace(self.timestamps[-1], self.timestamps[-1] + 15, num=15).reshape(-1, 1)
+        future_y = model_y.predict(future_times)
+
+        last_x = self.positions[-1][0]
+        last_x = np.full((15, 1), last_x)
+
+        self.predicted_positions = np.column_stack((last_x, future_y))
 
 
 class CrossingNode(Node):
@@ -90,13 +103,13 @@ class CrossingNode(Node):
         self.safe_to_cross = False
         self.traffic_light_state = None
         self.safety_counter = 0
+        self.stop_robot = False  # New flag to stop the robot
+        self.started_crossing = False
 
-        # Initialize the plot for displaying the trajectories of the tracked objects
         plt.ion()
         self.fig, self.ax = plt.subplots()
         self.ax.set_title("Tracked Objects' Trajectories")
         plt.show()
-
 
     def update_plot(self):
         self.ax.clear()
@@ -108,27 +121,24 @@ class CrossingNode(Node):
             if len(obj.positions) > 1:
                 data = np.array(obj.positions)
                 self.ax.plot(data[:, 0], data[:, 1], marker='o', label=f"Object {obj.id}")
+            if len(obj.predicted_positions) > 0:
+                predicted_data = np.array(obj.predicted_positions)
+                self.ax.scatter(predicted_data[:, 0], predicted_data[:, 1], marker='x', label=f"Object {obj.id} Prediction")
 
-        # Plot a line at y = 37 to represent the robot path in the crosswalk
         self.ax.axhline(y=37, color='r', linestyle='--', label='Robot Desired Path')
 
         self.ax.legend()
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
-
-
     def traffic_light_callback(self, msg):
         if not self.crossing:
             return
         self.traffic_light_state = msg.colour
-        # self.get_logger().info(f"Traffic light state updated to: {msg.colour}")
-
 
     def odom_callback(self, msg):
         self.robot_pose = msg.pose.pose
         self.check_crosswalk_proximity()
-
 
     def check_crosswalk_proximity(self):
         if self.robot_pose is None:
@@ -141,35 +151,28 @@ class CrossingNode(Node):
             self.crossing = True
         elif distance_to_exit < 1.0:
             self.crossing = False
-
+            self.started_crossing = False
 
     def occupancy_grid_callback(self, msg):
         if not self.crossing:
             return
-        
-        # In this grid, the first index is the y coordinate and the second index is the x coordinate
-        self.height = msg.info.height # 200 (cells)
-        self.width = msg.info.width # 200 (cells)
-        self.resolution = msg.info.resolution # 0.5 (m)
-        self.grid_origin_x = msg.info.origin.position.x # -50 (m)
-        self.grid_origin_y = msg.info.origin.position.y # -50 (m)
-        self.grid = np.array(msg.data).reshape((self.height, self.width))
-        # The point (0,0) in world coordinates is at the center of the grid (y=100, x=100) grid coordinates
 
-        # Extract the road area from the occupancy grid
+        self.height = msg.info.height
+        self.width = msg.info.width
+        self.resolution = msg.info.resolution
+        self.grid_origin_x = msg.info.origin.position.x
+        self.grid_origin_y = msg.info.origin.position.y
+        self.grid = np.array(msg.data).reshape((self.height, self.width))
+
         x_min_grid = int((13 - self.grid_origin_x) / self.resolution)
         x_max_grid = int((28 - self.grid_origin_x) / self.resolution)
         road_area = self.grid[:, x_min_grid:x_max_grid]
         occupied = road_area > 0
 
-
-
-        # Perform DBSCAN clustering on the occupied cells
         if np.any(occupied):
             xy_points = np.column_stack(np.nonzero(occupied))
             clustering = DBSCAN(eps=2, min_samples=2).fit(xy_points)
             labels = clustering.labels_
-
 
             for k in set(labels):
                 if k == -1:
@@ -178,52 +181,37 @@ class CrossingNode(Node):
                 xy_cluster = xy_points[class_member_mask]
                 centroid = np.mean(xy_cluster, axis=0)
 
-                # Convert the centroid to world coordinates
                 x_world = (centroid[1] + x_min_grid) * self.resolution + self.grid_origin_x
                 y_world = centroid[0] * self.resolution + self.grid_origin_y
                 centroid_world = np.array([x_world, y_world])
                 self.track_objects(centroid_world)
 
-
         self.times_ran += 1
         self.safe_to_cross_check()
         self.update_plot()
 
-        # Log the velocities and accelerations of the tracked objects
-        for obj in self.tracked_objects:
-            if len(obj.velocities) > 0:
-                self.get_logger().info(f"Object {obj.id} - Velocity: {obj.velocity:.2f} m/s, Acceleration: {obj.acceleration:.2f} m/s^2")
-
-
-        # Check if any tracked object has not been seen for a while
         for obj in self.tracked_objects:
             obj.steps_since_seen += 1
             if obj.steps_since_seen > 40:
                 self.tracked_objects.remove(obj)
 
-        
-        # Count how many times in a row safe_to_cross is True
         if self.safe_to_cross:
             self.safety_counter += 1
         else:
             self.safety_counter = 0
 
-        if self.times_ran > 100 and self.safety_counter > 10:
-            # self.get_logger().info("Safe to cross the road")
-            # Publish a path message with the exit point as the only waypoint
+        if self.times_ran > 50 and self.safety_counter > 10 and not self.stop_robot:
             path_msg = Path()
             pose = PoseStamped()
             pose.pose.position.x = self.crosswalk_exit[0]
             pose.pose.position.y = self.crosswalk_exit[1]
             path_msg.poses.append(pose)
-            # self.publisher.publish(path_msg)
+            self.publisher.publish(path_msg)
+            self.started_crossing = True
 
+        if self.started_crossing:
+            self.check_and_handle_crossing_objects()
 
-
-    # This will continuously check if it is safe to cross the road
-    # It should consider safe to cross if:
-    # Traffic light is green
-    # All tracked objects are stopped or their distance to the robot is increasing
     def safe_to_cross_check(self):
         if self.traffic_light_state == 'green':
             self.safe_to_cross = True
@@ -234,9 +222,6 @@ class CrossingNode(Node):
         else:
             self.safe_to_cross = False
 
-
-
-    # The centroid comes in world coordinates and in the proper order (x, y)
     def track_objects(self, centroid):
         current_time = self.get_clock().now()
         timestamp = current_time.nanoseconds / 1e9
@@ -245,9 +230,7 @@ class CrossingNode(Node):
 
         max_history = 10
         decay_rate = 0.9
-        # The higher the decay rate, the more recent positions will be weighted more heavily
 
-        # Check if the centroid is close to an existing tracked object
         for obj in self.tracked_objects:
             recent_positions = obj.positions[-max_history:]
             num_positions = len(recent_positions)
@@ -261,39 +244,67 @@ class CrossingNode(Node):
                 obj.add_position(centroid, timestamp)
                 obj.steps_since_seen = 0
 
-
                 if len(obj.positions) > 10:
-                    data = np.array(obj.positions)
-                    X, y = data[:, 0].reshape(-1, 1), data[:, 1]
-                    model = LinearRegression().fit(X, y)
-
-                    # Predict the next position
-                    next_x = data[-1, 0] + np.mean(np.diff(data[:, 0]))
-                    next_y = model.predict([[next_x]])[0]
-                    next_position = np.array([next_x, next_y])
-                    obj.next_position = next_position
-
                     obj.update_velocity_and_acceleration(time_diff)
                     obj.distances_to_robot.append(np.linalg.norm(centroid - np.array([self.robot_pose.position.x, self.robot_pose.position.y])))
 
                     if len(obj.distances_to_robot) >= 5:
                         distances = np.array(obj.distances_to_robot[-5:])
-                        average = np.mean(distances)
-                        if obj.distances_to_robot[-1] > average:
+                        if obj.distances_to_robot[-1] > obj.distances_to_robot[0]:
                             obj.is_approaching = False
                         else:
                             obj.is_approaching = True
 
-
                 return
 
-        
         new_obj = TrackedObject(self.tracked_objects_id)
         new_obj.positions.append(centroid)
+        new_obj.timestamps.append(timestamp)
         self.tracked_objects_id += 1
         self.tracked_objects.append(new_obj)
 
+    def check_and_handle_crossing_objects(self):
+        if self.robot_pose is None:
+            return
+        
+        robot_x = self.robot_pose.position.x
+        robot_y = self.robot_pose.position.y
+        if not self.stop_robot:
+            for obj in self.tracked_objects:
+                if len(obj.predicted_positions) > 0:
+                    future_positions = np.array(obj.predicted_positions)
+                    if np.any(future_positions[:, 1] > 37) and np.any(future_positions[:, 1] < 37) and np.all(future_positions[:, 0] > robot_x + 3):
+                        # The robot should stop at its current position or at (20,37) if robot's x position is less than 20
+                        path_msg = Path()
+                        pose = PoseStamped()
+                        pose.pose.position.x = 20.0 if robot_x < 20 else robot_x
+                        pose.pose.position.y = robot_y
+                        path_msg.poses.append(pose)
+                        self.publisher.publish(path_msg)
+                        self.get_logger().info(f"Robot stopping due to object {obj.id} crossing path")
 
+                        self.stop_robot = True  # Set flag to stop robot
+                        return
+
+        # Check if the robot should resume
+        if self.stop_robot:
+            all_objects_passed = True
+            for obj in self.tracked_objects:
+                future_positions = np.array(obj.predicted_positions)
+                # If 
+                if np.any(future_positions[:, 1] > 37) and np.any(future_positions[:, 1] < 37) and np.all(future_positions[:, 0] > robot_x):
+                    all_objects_passed = False
+                    break
+
+            if all_objects_passed:
+                path_msg = Path()
+                pose = PoseStamped()
+                pose.pose.position.x = self.crosswalk_exit[0]
+                pose.pose.position.y = self.crosswalk_exit[1]
+                path_msg.poses.append(pose)
+                self.publisher.publish(path_msg)
+                self.get_logger().info(f"Robot resuming after all objects passed")
+                self.stop_robot = False  # Reset flag to allow robot to move
 
 
 def main(args=None):
@@ -301,6 +312,8 @@ def main(args=None):
     node = CrossingNode()
     rclpy.spin(node)
     node.destroy_node()
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
